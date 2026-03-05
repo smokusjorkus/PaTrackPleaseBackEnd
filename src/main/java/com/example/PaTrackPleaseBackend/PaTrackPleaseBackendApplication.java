1c00de43027cb9a41a18a5d6756e939ba95b9d4f
LAB24_USER ON WS25 <L24Y08W25@LabHCI.net>
2026-03-05 10:06:15 +0800

    first commit

---
+++ src/main/java/com/example/PaTrackPleaseBackend/PaTrackPleaseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.PaTrackPleaseBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaTrackPleaseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaTrackPleaseBackendApplication.class, args);
+	}
+
+}
