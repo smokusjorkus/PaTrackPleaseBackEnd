9f65b5ed583a01698f70ac312bd95111444ef5cc
smokusjorkus <johannelumauag11@gmail.com>
2026-03-24 20:08:10 +0800

    Added a foreign key to the user_id so that they are connected

---
+++ src/main/java/com/example/PaTrackPleaseBackend/Tasks/Model/Tasks.java
@@ -0,0 +1,82 @@
+package com.example.PaTrackPleaseBackend.Tasks.Model;
+
+import java.time.LocalDate;
+
+import com.example.PaTrackPleaseBackend.User.Model.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tasks")
+public class Tasks {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column(nullable = true)
+    private String taskName;
+    
+    @Column(nullable= true)
+    private String taskDescription;
+
+    @Column(nullable=true, unique=false)
+    private LocalDate dueDate;
+
+    @Column(nullable=true)
+    private String status;
+
+    public Tasks(){}
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id", nullable=false)
+    private User user;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long Id) {
+        this.Id = Id;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public String getTaskDescription() {
+        return taskDescription;
+    }
+
+    public void setTaskDescription(String taskDescription) {
+        this.taskDescription = taskDescription;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
